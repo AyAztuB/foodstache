fa6b137af5a1136029616012949a437b2e845d63
Leila NUNES DE BARROS <leila.nunes-de-barros@epita.fr>
2023-03-14 15:26:34 -0400

    debut barre nav

---
+++ Foodstache/app/src/main/java/com/example/foodstache/BottomNavigation.kt
@@ -0,0 +1,70 @@
+package com.example.foodstache
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.foodstache.databinding.ActivityBottomNavigationBinding
+
+class BottomNavigation : AppCompatActivity() {
+
+    //private lateinit var binding: ActivityBottomNavigationBinding
+    private lateinit var textView: TextView
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.nav_home -> {
+                textView.setText("Home")
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_videos-> {
+                textView.setText("Video")
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_add -> {
+                textView.setText("Add")
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_recette -> {
+                textView.setText("Recette")
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_chat -> {
+                textView.setText("Chat")
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bottom_navigation)
+        //binding = ActivityBottomNavigationBinding.inflate(layoutInflater)
+        //setContentView(binding.root)
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+        
+
+       /* val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_bottom_navigation)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)*/
+    }
+}
