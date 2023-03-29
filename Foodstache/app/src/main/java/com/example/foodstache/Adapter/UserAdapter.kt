6bb7a621aafe177e1565085fad59101f3153d5ad
Marie Eveillé <eveille.marie29@gmail.com>
2023-03-28 21:50:31 -0400

    ça avance

---
+++ Foodstache/app/src/main/java/com/example/foodstache/Adapter/UserAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.foodstache.Adapter
+
+import android.content.Context
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.foodstache.Model.User
+import com.example.foodstache.ProfileActivity
+
+class UserAdapter (private var mContext : Context,
+                   private var mUser : List<User>,
+                   private var isFragment : Boolean = false) : RecyclerView.Adapter<RecyclerView.ViewHolder>()
+{
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+
+}
--- Foodstache/app/src/main/java/com/example/foodstache/Model/User.kt
+++ Foodstache/app/src/main/java/com/example/foodstache/Model/User.kt
@@ -1,4 +1,81 @@
 package com.example.foodstache.Model
 
 class User {
+
+    private var username : String = ""
+    private var bio : String = ""
+    private var nbFollowing : String = ""
+    private var nbFollowers : String = ""
+    private var nbPost : String = ""
+    private var pp : String = ""
+    private var userId : String = ""
+
+    constructor()
+
+    constructor(username : String, bio : String, nbFollowing : String, nbFollowers : String, nbPost : String, pp : String, userId: String){
+        this.username = username
+        this.bio = bio
+        this.nbFollowing = nbFollowing
+        this.nbFollowers = nbFollowers
+        this.nbPost = nbPost
+        this.pp = pp
+        this.userId = userId
+    }
+
+    fun getUsername() : String{
+        return username
+    }
+
+    fun setUsername(username: String){
+        this.username= username
+    }
+    fun getBio() : String{
+        return bio
+    }
+
+    fun setBio(bio: String){
+        this.bio= bio
+    }
+
+    fun getNbFollowing() : String{
+        return nbFollowing
+    }
+
+    fun setNbFollowing(nbFollowing: String){
+        this.nbFollowing= nbFollowing
+    }
+
+    fun getNbFollowers() : String{
+        return nbFollowers
+    }
+
+    fun setNbFollowers(nbFollowers: String){
+        this.nbFollowers= nbFollowers
+    }
+
+    fun getNbPost() : String{
+        return nbPost
+    }
+
+    fun setNbPost(nbPost: String){
+        this.nbPost= nbPost
+    }
+
+    fun getPp() : String{
+        return pp
+    }
+
+    fun setPp(pp: String){
+        this.pp= pp
+    }
+
+    fun getUserId() : String{
+        return userId
+    }
+
+    fun setUserId(userId: String){
+        this.userId= userId
+    }
+
+
 }
