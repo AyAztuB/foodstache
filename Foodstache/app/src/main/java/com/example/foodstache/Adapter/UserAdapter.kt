7084cbed60ab3ef8adce91b4f071399fb8c90673
Marie Eveillé <eveille.marie29@gmail.com>
2023-04-01 01:06:42 -0400

    ça avance encore, et j'ai un pb mais il est trop tard pour que j'arrive à le résoudre, bonne nuit les petits

--- Foodstache/app/src/main/java/com/example/foodstache/Adapter/UserAdapter.kt
+++ Foodstache/app/src/main/java/com/example/foodstache/Adapter/UserAdapter.kt
@@ -1,33 +1,48 @@
 package com.example.foodstache.Adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.annotation.NonNull
 import androidx.recyclerview.widget.RecyclerView
 import com.example.foodstache.Model.User
 import com.example.foodstache.R
+import com.squareup.picasso.Picasso
 
 
 class UserAdapter (private var mContext : Context,
                    private var mUser : List<User>,
                    private var isFragment : Boolean = false) : RecyclerView.Adapter<UserAdapter.ViewHolder>()
 {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserAdapter.ViewHolder {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        var view = LayoutInflater.from(mContext).inflate(R.layout.search_layout, parent, false)
+        return ViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return mUser.size
     }
 
-    override fun onBindViewHolder(holder: UserAdapter.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val user = mUser[position]
+        holder.userNameTextView.text = user.getUsername()
+        holder.fullNameTextView.text = user.getFullName()
+        Picasso.get().load(user.getPp()).placeholder(R.drawable.profile_image).into(holder.profilPictureImageView)
     }
 
-    class ViewHolder (@NonNull itemView : android.view.View) : RecyclerView.ViewHolder(itemView){
+    class ViewHolder (@NonNull itemView : View) : RecyclerView.ViewHolder(itemView){
         var userNameTextView: TextView = itemView.findViewById(R.id.user_name_id_search)
+        var fullNameTextView: TextView = itemView.findViewById(R.id.full_name)
+        var profilPictureImageView: ImageView = itemView.findViewById(R.id.profile_image)
+        var bioTextView: TextView = itemView.findViewById(R.id.bio)
+        var totalPostsTextView: TextView = itemView.findViewById(R.id.total_posts)
+        var totalFollowersTextView: TextView = itemView.findViewById(R.id.total_followers)
+        var totalFollowingTextView: TextView = itemView.findViewById(R.id.total_following)
+        var followButton : Button = itemView.findViewById(R.id.followButton)
     }
 
 
--- Foodstache/app/src/main/java/com/example/foodstache/Fragments/VideoFragment.kt
+++ Foodstache/app/src/main/java/com/example/foodstache/Fragments/VideoFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.foodstache.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.foodstache.Adapter.*
+import com.example.foodstache.Model.*
+import com.example.foodstache.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,43 +22,30 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class VideoFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+
+    private var recyclerView: RecyclerView? = null
+    private var userAdapter : UserAdapter? = null
+    private var mUser : MutableList<User>? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_video, container, false)
+        val view = inflater.inflate(R.layout.fragment_video, container, false)
+
+        recyclerView = view.findViewById(R.id.recycler_view_search)
+        recyclerView?.setHasFixedSize(true)
+        recyclerView?.layoutManager = LinearLayoutManager(context)
+
+        mUser = ArrayList()
+        userAdapter = context?.let{UserAdapter(it, mUser as ArrayList<User>, true)}
+        recyclerView?.adapter = userAdapter
+
+
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment VideoFragment1.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            VideoFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
