4fa6e077198fa03ea279cd989206e301de4bfd7e
Abel-moi <59056841+Abel-moi@users.noreply.github.com>
2023-04-28 16:46:26 -0400

    debut notif pour meline

--- Foodstache/app/src/main/java/com/example/foodstache/ChatActivity.kt
+++ Foodstache/app/src/main/java/com/example/foodstache/ChatActivity.kt
@@ -1,11 +1,20 @@
 package com.example.foodstache
 
+import android.Manifest
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.ImageView
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.foodstache.Fragments.ChatFragment
@@ -15,6 +24,10 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.logging.SimpleFormatter
+import kotlin.collections.ArrayList
 
 class ChatActivity : AppCompatActivity() {
 
@@ -26,12 +39,15 @@ class ChatActivity : AppCompatActivity() {
     private lateinit var mDbRef: DatabaseReference
     private lateinit var backBtn : ImageButton
 
+    var notification: Int = 0
     var receiverRoom: String? = null
     var senderRoom: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+
+        val notification=intent.getStringExtra("notif_activated")
 
         val name = intent.getStringExtra("name")
         val receiverUid = intent.getStringExtra("uid")
@@ -92,8 +108,58 @@ class ChatActivity : AppCompatActivity() {
                     mDbRef.child("chats").child(receiverRoom!!).child("messages").push()
                         .setValue(messageObject)
                 }
+            Notification()
             messageBox.setText("")
 
         }
+    }
+
+    private fun Notification(){
+         CreateNotificationChannel()
+            val date=Date()
+            val notificationId=SimpleDateFormat("ddHHmmss", Locale.CANADA).format(date).toInt()
+
+            val notificationBuilder=NotificationCompat.Builder(this, "$CHANNEL_ID")
+
+            notificationBuilder.setSmallIcon(R.drawable.notification_icon)
+            notificationBuilder.setContentTitle("New message")
+            notificationBuilder.setContentText("$senderRoom sent you a message")
+            notificationBuilder.priority=NotificationCompat.PRIORITY_DEFAULT
+            notificationBuilder.setAutoCancel(true)
+
+            val notificationManagerCompat=NotificationManagerCompat.from(this)
+
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return
+            }
+            notificationManagerCompat.notify(notificationId, notificationBuilder.build())
+
+    }
+
+    private fun CreateNotificationChannel(){
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            val name:CharSequence="MyNotification"
+            val description="My notification channel description"
+
+            val importance=NotificationManager.IMPORTANCE_DEFAULT
+            val notificationChannel= NotificationChannel(CHANNEL_ID, name, importance)
+            val notificationManager=getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+    }
+
+    private companion object{
+        private const val CHANNEL_ID="notification_channel"
     }
 }
